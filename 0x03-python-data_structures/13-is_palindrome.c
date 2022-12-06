0a6fd0887fc794d558e73037b6f7d33402331c32
Kylevnrn <kylevnrn@gmail.com>
2022-12-06 09:48:29 -0800

    Task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 #include <string.h>
 #include "lists.h"
 /**
- * is_palindrome - ispalindrome
- * @head: list
- * Return: number elements.
+ *is_palindrome - ispalindrome
+ *@head: list
+ *Return: number elements.
  */
 int is_palindrome(listint_t **head)
 {
